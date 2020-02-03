9343dfb1652c5ca374fa5ebd4e412ff8287bd52d
Artexacast <jason_matthews09@hotmail.co.uk>
2020-02-03 23:21:31 +0000

    bcrypt issue resolved with new version of NodeJS
    Need to run npm install --global --production windows-build-tools
    
    install .NET SDK 2.0

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const user = require('./models/User');
+const bcrypt = require('bcrypt');
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -11,6 +14,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 const port = process.env.PORT || 5000;
 
 app.post('/signup', (req,res)=>{
+
+    const newUser = new user({
+        name: req.body.name,
+        email: req.body.email,
+        //password storage, bcrypt(value, saltRounds ) salt is stored
+        password: bcrypt.hashSync(req.body.password, 5)
+    })
     console.log(req.body);
 })
 
