70706e8833ca753a45e32d925b481603db1bf865
Artexacast <jason_matthews09@hotmail.co.uk>
2020-02-03 20:52:51 +0000

    Input from fields working

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,14 +1,31 @@
 <template>
     <div>
-        NAME:<input type="text" /><br>
-        EMAIL:<input type="text" /><br>
-        PASSWORD:<input type="text" /><br>
+        <!-- v-model is for input bindings -->
+        NAME:<input type="text" v-model="name"/><br>
+        EMAIL:<input type="text" v-model="email"/><br>
+        PASSWORD:<input type="text" v-model="password"/><br>
+        <!-- call signup function -->
+        <button @click="signup">Sign up</button>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Signup'
+    name: 'Signup',
+    data(){
+        return{
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        signup(){
+            console.log(this.name);
+            console.log(this.email);
+            console.log(this.password);
+        }
+    }
 }
 </script>
 
